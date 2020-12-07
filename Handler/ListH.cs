2fdd74d52ea8eec79b48c94fd114e589596adffd
Purity <66000596+graalpurity@users.noreply.github.com>
2020-12-07 16:57:16 -0500

    Pog Handlers

---
+++ Handler/ListH.cs
@@ -0,0 +1,31 @@
+﻿//this is used for handling the Script List 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Slurp_Juice;
+
+namespace Slurp_Juice.Handler
+{
+    public static class ListH
+    {
+        public static void Clear()
+        {
+            Slurp form = new Slurp();
+            form.scriptsList.Items.Clear();
+        }
+
+        public static void PopulateListBox(ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name);
+            }
+        }
+    }
+}
